c4ee474475992006b54838a5625ebb959b75d87e
Ubuntu <ubuntu@ip-172-31-20-61.ap-southeast-2.compute.internal>
2026-05-09 12:33:21 +0000

    fixed app

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,30 +1,40 @@
 package com.devops;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
 
 public class App {
-
     public static void main(String[] args) throws Exception {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        server.createContext("/", exchange -> {
+        server.createContext("/", new MyHandler());
 
-            String response = "Java App Running Successfully!";
+        server.setExecutor(null);
+
+        server.start();
+
+        System.out.println("Java App Running...");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            String response = "Hello from Java DevOps App";
 
             exchange.sendResponseHeaders(200, response.length());
 
             OutputStream os = exchange.getResponseBody();
+
             os.write(response.getBytes());
+
             os.close();
-        });
-
-        server.setExecutor(null);
-        server.start();
-
-        System.out.println("Server started on port 8080");
+        }
     }
 }
