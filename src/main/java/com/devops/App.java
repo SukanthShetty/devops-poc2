721af0232263c7a1469f477e7704a4e5a0652ff5
Ubuntu <ubuntu@ip-172-31-20-61.ap-southeast-2.compute.internal>
2026-05-09 12:03:20 +0000

    fixed pom

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,10 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java App Running...");
+        while(true){
+            Thread.sleep(1000);
+        }
+    }
+}
