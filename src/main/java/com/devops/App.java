7a5d08ac75424c91056eb5d4cb978570704faed5
Ubuntu <ubuntu@ip-172-31-20-61.ap-southeast-2.compute.internal>
2026-05-09 12:22:52 +0000

    fixed java web app

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,10 +1,30 @@
 package com.devops;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Java App Running...");
-        while(true){
-            Thread.sleep(1000);
-        }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+
+            String response = "Java App Running Successfully!";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
 }
