3e1377615444e23cc1b047b7f2a9c1820d83c07e
SukanthShetty <sukanth.rshetty@hcltech.com>
2026-05-12 14:30:45 +0530

    Update java.App.java

--- src/main/java.App.java
+++ src/main/java.App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from sukanth");
+        System.out.println("Hello from sukanth R Shetty");
     }
 }
