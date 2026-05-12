025ea4e2e9b5ba9cf9a8789a4a62a29cf5b92f58
SukanthShetty <sukanth.rshetty@hcltech.com>
2026-05-12 15:09:19 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -26,7 +26,7 @@ public class App {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
 
-            String response = "Hello from sukanth rVsHETTY";
+            String response = "Hello from sukanth rVs";
 
             exchange.sendResponseHeaders(200, response.length());
 
